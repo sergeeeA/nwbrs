f583246091d39e246f4e96b388d78a8303cb31ee
Your Name <sergeialeks232@gmail.com>
2024-07-29 07:57:29 +0800

    first commit

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,3 @@
 import abi from './Lottery.json'
 export const lotteryAbi = abi.abi
-export const LotteryAddress = '0x73AEf6823B9F7E125593e58b8CC72098ddCb1869'
+export const LotteryAddress = '0x68260fa1751af6FB40b28278eA39f2F44E42d835'
